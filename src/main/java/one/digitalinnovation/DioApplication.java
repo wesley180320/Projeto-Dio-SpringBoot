5f486fc279623643bbb427ed635d70f81260c1fc
wesley180320 <estevaoowesley@gmail.com>
2022-01-04 20:44:46 -0400

    Inicio do projeto

---
+++ src/main/java/one/digitalinnovation/DioApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioApplication.class, args);
+	}
+
+}
